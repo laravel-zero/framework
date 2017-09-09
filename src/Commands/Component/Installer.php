e39914c1a0012661fae6678e7d18697bdb78a5be
Nuno Maduro <enunomaduro@gmail.com>
2017-09-10 00:28:40 +0200

    Removes header

--- src/Commands/Component/Installer.php
+++ src/Commands/Component/Installer.php
@@ -1,14 +1,5 @@
 <?php
 
-/**
- * This file is part of Zero Framework.
- *
- * (c) Nuno Maduro <enunomaduro@gmail.com>
- *
- *  For the full copyright and license information, please view the LICENSE
- *  file that was distributed with this source code.
- */
-
 namespace LaravelZero\Framework\Commands\Component;
 
 use Symfony\Component\Console\Input\InputArgument;
