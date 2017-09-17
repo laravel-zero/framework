22ccd278697993e7fc97b7a3e865649866e4edc8
Nuno Maduro <enunomaduro@gmail.com>
2017-09-17 12:22:01 +0200

    Adds Laravel Core helpers

---
+++ src/helpers.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Container\Container;
+
+if (! function_exists('app')) {
+    /**
+     * Get the available container instance.
+     *
+     * @param  string  $make
+     * @return mixed|\Laravel\Lumen\Application
+     */
+    function app($make = null)
+    {
+        if (is_null($make)) {
+            return Container::getInstance();
+        }
+
+        return Container::getInstance()->make($make);
+    }
+}
+
+if (! function_exists('config')) {
+    /**
+     * Get / set the specified configuration value.
+     *
+     * If an array is passed as the key, we will assume you want to set an array of values.
+     *
+     * @param  array|string  $key
+     * @param  mixed  $default
+     * @return mixed
+     */
+    function config($key = null, $default = null)
+    {
+        if (is_null($key)) {
+            return app('config');
+        }
+
+        if (is_array($key)) {
+            return app('config')->set($key);
+        }
+
+        return app('config')->get($key, $default);
+    }
+}
+
+if (! function_exists('event')) {
+    /**
+     * Fire an event and call the listeners.
+     *
+     * @param  object|string  $event
+     * @param  mixed   $payload
+     * @param  bool    $halt
+     * @return array|null
+     */
+    function event($event, $payload = [], $halt = false)
+    {
+        return app('events')->fire($event, $payload, $halt);
+    }
+}
