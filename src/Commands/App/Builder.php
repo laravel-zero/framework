efa5e48f66c6bbc6c8c8ee2a9ca2c3b595c9f232
KwangSeob Jeong <lesstif@gmail.com>
2017-11-30 09:39:34 +0900

    fixed build failed on windows and checked running correctly.

--- src/Commands/App/Builder.php
+++ src/Commands/App/Builder.php
@@ -100,8 +100,16 @@ class Builder extends Command
             ->getCompiler($name);
 
         $structure = config('app.structure') ?: $this->structure;
+        
+        // See - https://github.com/laravel-zero/framework/pull/102
+        $regex = '#'.implode('|', $structure).'#';
 
-        $compiler->buildFromDirectory(BASE_PATH, '#'.implode('|', $structure).'#');
+        if (stristr(PHP_OS, "WINNT") != FALSE) {
+            $compiler->buildFromDirectory(BASE_PATH, str_replace('\\', '/', $regex));
+        } else {
+            // Linux, OS X
+            $compiler->buildFromDirectory(BASE_PATH, $regex);
+        }
         $compiler->setStub(
             "#!/usr/bin/env php \n".$compiler->createDefaultStub('bootstrap'.DIRECTORY_SEPARATOR.'init.php')
         );
