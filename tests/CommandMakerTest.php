77696bab7d194f53786498dbcd4663b65765330d
Nuno Maduro <enunomaduro@gmail.com>
2018-01-04 23:34:18 +0100

    Fixes command maker test

--- tests/CommandMakerTest.php
+++ tests/CommandMakerTest.php
@@ -8,15 +8,15 @@ class CommandMakerTest extends TestCase
 {
     public function tearDown()
     {
-        File::delete(app_path('commands' . DIRECTORY_SEPARATOR . 'FooCommand.php'));
+        File::delete(app_path('Commands' . DIRECTORY_SEPARATOR . 'FooCommand.php'));
     }
 
     /** @test */
-    public function it_builds_the_application(): void
+    public function it_creates_commands(): void
     {
         $this->app->call('make:command', ['name' => 'FooCommand']);
 
-        $file = app_path('commands'.DIRECTORY_SEPARATOR.'FooCommand.php');
+        $file = app_path('Commands'.DIRECTORY_SEPARATOR.'FooCommand.php');
 
         $this->assertTrue(File::exists($file));
         $this->assertContains('class FooCommand extends Command', File::get($file));
