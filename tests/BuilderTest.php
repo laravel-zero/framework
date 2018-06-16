153e1206df7a59f85843555628699245121f0d60
Nuno Maduro <enunomaduro@gmail.com>
2018-06-16 23:16:28 +0200

    Fixes tests after new box feature

--- tests/BuilderTest.php
+++ tests/BuilderTest.php
@@ -4,6 +4,7 @@ namespace Tests;
 
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Artisan;
+use Symfony\Component\Console\Output\NullOutput;
 use LaravelZero\Framework\Contracts\Providers\Composer;
 
 class BuilderTest extends TestCase
@@ -17,14 +18,25 @@ class BuilderTest extends TestCase
     public function it_builds_the_application(): void
     {
         $composerMock = $this->createMock(Composer::class);
-        $composerMock->expects($this->exactly(4))->method('install');
         $this->app->instance(Composer::class, $composerMock);
 
-        Artisan::call('app:build');
+        $output = new class() extends NullOutput {
+            public function section()
+            {
+                return new class() extends NullOutput {
+                    public function clear()
+                    {
+                    }
+                };
+            }
+        };
+
+
+        Artisan::call('app:build', [], $output);
 
         $this->assertTrue(File::exists(base_path('builds'.DIRECTORY_SEPARATOR.'application')));
 
-        Artisan::call('app:build', ['name' => 'zonda']);
+        Artisan::call('app:build', ['name' => 'zonda'], $output);
 
         $this->assertTrue(File::exists(base_path('builds'.DIRECTORY_SEPARATOR.'zonda')));
     }
