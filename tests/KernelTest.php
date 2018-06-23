8930648f74ddb77390c9fc2d0298b2558efe038e
Nuno Maduro <enunomaduro@gmail.com>
2018-06-24 00:00:38 +0200

    Tests against output bind

---
+++ tests/KernelTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use Illuminate\Support\Facades\Artisan;
+use LaravelZero\Framework\Application;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\NullOutput;
+use Symfony\Component\Console\Output\OutputInterface;
+
+final class KernelTest extends TestCase
+{
+    /** @test */
+    public function it_binds_the_output_into_the_container(): void
+    {
+        Artisan::handle(new ArrayInput([]), $output = new NullOutput);
+
+        $this->assertEquals(
+            $output,
+            Application::getInstance()
+                ->get(OutputInterface::class)
+        );
+    }
+}
