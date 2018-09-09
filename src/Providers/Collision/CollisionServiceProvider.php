ef59dab8714bfdf9075d00860d3f1aedd4e11152
Nuno Maduro <enunomaduro@gmail.com>
2018-09-09 21:01:13 +0200

    Registers collision only in production

---
+++ src/Providers/Collision/CollisionServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of Laravel Zero.
+ *
+ * (c) Nuno Maduro <enunomaduro@gmail.com>
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ */
+
+namespace LaravelZero\Framework\Providers\Collision;
+
+use NunoMaduro\Collision\Adapters\Laravel\CollisionServiceProvider as BaseCollisionServiceProvider;
+
+final class CollisionServiceProvider extends BaseCollisionServiceProvider
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function register(): void
+    {
+        if (! $this->app->environment('production')) {
+            $this->app->register(BaseCollisionServiceProvider::class);
+        }
+    }
+}
