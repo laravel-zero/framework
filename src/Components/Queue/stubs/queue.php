1e86518297ddd9bec92ab7fd6bd671f55d4911af
Nuno Maduro <enunomaduro@gmail.com>
2018-09-12 22:59:39 +0200

    Failed jobs defaults to sqlite

--- src/Components/Queue/stubs/queue.php
+++ src/Components/Queue/stubs/queue.php
@@ -79,7 +79,7 @@ return [
     */
 
     'failed' => [
-        'database' => env('DB_CONNECTION', 'mysql'),
+        'database' => env('DB_CONNECTION', 'sqlite'),
         'table' => 'failed_jobs',
     ],
 
