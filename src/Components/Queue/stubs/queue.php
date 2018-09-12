d73346822cd731aa42275db131f3d5f1476442ae
Nuno Maduro <enunomaduro@gmail.com>
2018-09-12 22:47:35 +0200

    Updates stubs

--- src/Components/Queue/stubs/queue.php
+++ src/Components/Queue/stubs/queue.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('QUEUE_DRIVER', 'database'),
+    'default' => env('QUEUE_CONNECTION', 'database'),
 
     /*
     |--------------------------------------------------------------------------
