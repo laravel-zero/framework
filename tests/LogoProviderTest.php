09c3885a2372498c1fcf2826ce31effb1ce4d794
Victor Lap <victorlap@outlook.com>
2018-10-08 22:34:40 +0200

    Fix test

--- tests/LogoProviderTest.php
+++ tests/LogoProviderTest.php
@@ -4,25 +4,28 @@ declare(strict_types=1);
 
 namespace Tests;
 
-use Illuminate\Support\Facades\Artisan;
+use LaravelZero\Framework\Components\Logo\FigletString;
 
 final class LogoProviderTest extends TestCase
 {
-    public function testLogoReplacesAppName(): void
+    public function testLogoCanBeGenerated(): void
     {
         $logo = <<<LOGO
-                      _ _           _   _
-    /\               | (_)         | | (_)
-   /  \   _ __  _ __ | |_  ___ __ _| |_ _  ___  _ __
-  / /\ \ | '_ \| '_ \| | |/ __/ _` | __| |/ _ \| '_ \
+
+                      _ _           _   _             
+    /\               | (_)         | | (_)            
+   /  \   _ __  _ __ | |_  ___ __ _| |_ _  ___  _ __  
+  / /\ \ | '_ \| '_ \| | |/ __/ _` | __| |/ _ \| '_ \ 
  / ____ \| |_) | |_) | | | (_| (_| | |_| | (_) | | | |
 /_/    \_\ .__/| .__/|_|_|\___\__,_|\__|_|\___/|_| |_|
-         | |   | |
-         |_|   |_|
+         | |   | |                                    
+         |_|   |_|                                    
+
+
 LOGO;
 
-        $output = Artisan::getName();
+        $figlet = new FigletString('Application', []);
 
-        $this->assertContains($logo, $output);
+        $this->assertEquals($logo, (string) $figlet);
     }
 }
