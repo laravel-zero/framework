8f91a817ee23bb67111c28cacb16a2d992bf2ad1
Nuno Maduro <enunomaduro@gmail.com>
2019-03-10 18:15:25 +0000

    Apply fixes from StyleCI

--- tests/UpdaterInstallTest.php
+++ tests/UpdaterInstallTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Tests;
 
-use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Artisan;
 use LaravelZero\Framework\Contracts\Providers\ComposerContract;
 
