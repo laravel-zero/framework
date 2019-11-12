laravel-zero/framework

commit 0f6f302e877ea31aa81eae8550b8147b23798957

Autoload database factories

src/Components/Database/Provider.php
@@ -95,6 +95,14 @@ class Provider extends AbstractComponentProvider
                 }
             );
         }
+
+        if (File::exists($this->app->databasePath('factories'))) {
+            collect(File::files($this->app->databasePath('factories')))->each(
+                function ($file) {
+                    File::requireOnce($file);
+                }
+            );
+        }
     }
 
     /**

commit bbb38007a0b76f4a3b06d636e75a3f9b004c0c2f

Remove factories auto loader
This reverts a breaking change that was introduced in 0f6f302e877ea31aa81eae8550b8147b23798957

Closes laravel-zero/laravel-zero#247

src/Components/Database/Provider.php
@@ -96,14 +96,6 @@ class Provider extends AbstractComponentProvider
                 }
             );
         }
-
-        if (File::exists($this->app->databasePath('factories'))) {
-            collect(File::files($this->app->databasePath('factories')))->each(
-                function ($file) {
-                    File::requireOnce($file);
-                }
-            );
-        }
     }
 
     /**