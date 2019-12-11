83f3957f9bd4e92778d082e9ea34de0d9e3e354f
Owen Voke <owzie123@gmail.com>
2019-12-11 14:47:48 +0000

    Update to support Windows in the config

--- src/Components/ScheduleList/stubs/schedule-list.php
+++ src/Components/ScheduleList/stubs/schedule-list.php
@@ -2,7 +2,12 @@
 
 return [
     'remove_strings_from_command' => [
+        // Single quotes for Linux and MacOS
         "'".PHP_BINARY."'",
         "'%APPLICATION_NAME%'",
+
+        // Double quotes for Windows
+        '"'.PHP_BINARY.'"',
+        '"%APPLICATION_NAME%"',
     ],
 ];
--- tests/Application/config/schedule-list.php
+++ tests/Application/config/schedule-list.php
@@ -2,7 +2,12 @@
 
 return [
     'remove_strings_from_command' => [
+        // Single quotes for Linux and MacOS
         "'".PHP_BINARY."'",
         "'application'",
+
+        // Double quotes for Windows
+        '"'.PHP_BINARY.'"',
+        '"application"',
     ],
 ];
