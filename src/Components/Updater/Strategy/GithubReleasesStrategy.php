e92cdae4ae0ecb8ca3cc94cabf74c7cb9ce75015
Owen Voke <development@voke.dev>
2020-04-22 11:48:56 +0100

    Add download strategy for GitHub Releases

---
+++ src/Components/Updater/Strategy/GithubReleasesStrategy.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace LaravelZero\Framework\Components\Updater\Strategy;
+
+final class GithubReleasesStrategy extends \Humbug\SelfUpdate\Strategy\GithubStrategy implements StrategyInterface
+{
+}
