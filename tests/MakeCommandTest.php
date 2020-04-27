a76fdea8e69c2290561b24d48507d52b73ee2f4d
Owen Voke <development@voke.dev>
2020-04-27 15:05:20 +0100

    Add test for publishing stubs

--- tests/MakeCommandTest.php
+++ tests/MakeCommandTest.php
@@ -12,6 +12,8 @@ final class MakeCommandTest extends TestCase
     public function tearDown(): void
     {
         File::delete(app_path('Commands'.DIRECTORY_SEPARATOR.'FooCommand.php'));
+        File::delete(app_path('Commands/PublishedStubbedCommand.php'));
+        File::delete(base_path('stubs/console.stub'));
     }
 
     public function testCommandCreation(): void
@@ -22,5 +24,27 @@ final class MakeCommandTest extends TestCase
 
         $this->assertTrue(File::exists($file));
         $this->assertStringContainsString('class FooCommand extends Command', File::get($file));
+        $this->assertStringContainsString('use LaravelZero\Framework\Commands\Command;', File::get($file));
+    }
+
+    /** @test */
+    public function it_can_make_a_command_from_published_stub(): void
+    {
+        $consoleStubPath = base_path('stubs/console.stub');
+
+        Artisan::call('stub:publish');
+        $this->assertFileExists($consoleStubPath);
+
+        File::put(
+            $consoleStubPath,
+            str_replace('extends Command', 'extends Command implements FooInterface', File::get($consoleStubPath))
+        );
+
+        Artisan::call('make:command', ['name' => 'PublishedStubbedCommand']);
+        $file = app_path('Commands/PublishedStubbedCommand.php');
+
+        $this->assertFileExists($file);
+        $this->assertStringContainsString('class PublishedStubbedCommand extends Command implements FooInterface', File::get($file));
+        $this->assertStringContainsString('use LaravelZero\Framework\Commands\Command;', File::get($file));
     }
 }
