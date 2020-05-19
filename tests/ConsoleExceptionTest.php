da6f0d721ef05cacbeb1704c235cb4b602c3891d
Owen Voke <development@voke.dev>
2020-05-19 11:09:32 +0100

    Update ConsoleExceptionTest to Pest

--- tests/ConsoleExceptionTest.php
+++ tests/ConsoleExceptionTest.php
@@ -2,27 +2,20 @@
 
 declare(strict_types=1);
 
-namespace Tests;
-
 use LaravelZero\Framework\Exceptions\ConsoleException;
 
-final class ConsoleExceptionTest extends TestCase
-{
-    public function testExitCode(): void
-    {
-        $exception = new ConsoleException(404, 'Foo', ['bar' => 'etc']);
+it('sets console exception exit code', function () {
+    $exception = new ConsoleException(404, 'Foo', ['bar' => 'etc']);
 
-        $this->assertEquals(404, $exception->getExitCode());
-    }
+    assertEquals(404, $exception->getExitCode());
+});
 
-    public function testHeadersSetter(): void
-    {
-        $exception = new ConsoleException(404, 'Foo', ['bar' => 'etc']);
+it('sets console exception headers', function () {
+    $exception = new ConsoleException(404, 'Foo', ['bar' => 'etc']);
 
-        $this->assertEquals(['bar' => 'etc'], $exception->getHeaders());
+    assertEquals(['bar' => 'etc'], $exception->getHeaders());
 
-        $exception->setHeaders(['foo' => 'zero']);
+    $exception->setHeaders(['foo' => 'zero']);
 
-        $this->assertEquals(['foo' => 'zero'], $exception->getHeaders());
-    }
-}
+    assertEquals(['foo' => 'zero'], $exception->getHeaders());
+});
