7f915d9f07934a9dcd06d2fa51ffe45c4cad536b
Fabio Ivona <fabio.ivona@defstudio.it>
2020-08-27 15:19:49 +0200

    Update GitlabStrategy.php
    formatting fix

--- src/Components/Updater/Strategy/GitlabStrategy.php
+++ src/Components/Updater/Strategy/GitlabStrategy.php
@@ -15,12 +15,10 @@ namespace LaravelZero\Framework\Components\Updater\Strategy;
          */
         protected function getDownloadUrl(array $package): string
         {
-
             $downloadUrl = parent::getDownloadUrl($package);
 
             $downloadUrl = str_replace('releases/download', '-/raw', $downloadUrl);
 
             return $downloadUrl.'/builds/'.basename(Phar::running());
-
         }
     }
