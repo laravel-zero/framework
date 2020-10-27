fbe6ef74acbd5c62d2a2a418bad56e42e0945028
Owen Voke <development@voke.dev>
2020-10-27 15:26:12 +0000

    feat: add abstract class for direct download updates

---
+++ src/Components/Updater/Strategy/AbstractDirectDownloadStrategy.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace LaravelZero\Framework\Components\Updater\Strategy;
+
+use Humbug\SelfUpdate\Updater;
+use Humbug\SelfUpdate\Exception\HttpRequestException;
+
+abstract class AbstractDirectDownloadStrategy implements StrategyInterface
+{
+    /** {@inheritdoc} */
+    public function download(Updater $updater)
+    {
+        /** Switch remote request errors to HttpRequestExceptions */
+        set_error_handler(array($updater, 'throwHttpRequestException'));
+        $result = humbug_get_contents($this->getDownloadUrl());
+        restore_error_handler();
+        if (false === $result) {
+            throw new HttpRequestException(sprintf(
+                'Request to URL failed: %s',
+                $this->getDownloadUrl()
+            ));
+        }
+
+        file_put_contents($updater->getTempPharFile(), $result);
+    }
+
+    /** {@inheritdoc} */
+    public function getCurrentRemoteVersion(Updater $updater)
+    {
+        return 'latest';
+    }
+
+    /** {@inheritdoc} */
+    public function getCurrentLocalVersion(Updater $updater)
+    {
+        return $this->localVersion;
+    }
+
+    abstract public function getDownloadUrl(): string;
+}
