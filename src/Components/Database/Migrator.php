4c14c9e94a3f292d18e1e02cd5ec55866338426f
Yoram <yoramdelangen@gmail.com>
2020-12-23 07:49:11 +0100

    Added support for migrating single file
    Laravel framework supports this and I have some situations I need to migrate only a single file instead of all migrations within the migrations folder. 
    This can particularly when you have migrations within your build (phar) and migrations outside of the build.

--- src/Components/Database/Migrator.php
+++ src/Components/Database/Migrator.php
@@ -14,6 +14,7 @@ declare(strict_types=1);
 namespace LaravelZero\Framework\Components\Database;
 
 use function collect;
+use Illuminate\Support\Str;
 use Illuminate\Database\Migrations\Migrator as BaseMigrator;
 use SplFileInfo;
 use Symfony\Component\Finder\Finder;
@@ -35,10 +36,17 @@ class Migrator extends BaseMigrator
         return collect($paths)
             ->flatMap(
                 function ($path) {
-                    return collect(
-                        (new Finder)->in([$path])
+                    $finder = null;
+                    if (Str::endsWith($path, '.php')) {
+                        $finder = (new Finder)->in([dirname($path)])
                             ->files()
-                    )
+                            ->name(basename($path));
+                    } else {
+                        $finder = (new Finder)->in([$path])
+                            ->files();
+                    }
+
+                    return collect($finder)
                         ->map(
                             function (SplFileInfo $file) {
                                 return $file->getPathname();
