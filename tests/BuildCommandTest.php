af03fa8dffc28b6db4996ad325456972035af150
Owen Voke <owenvoke@users.noreply.github.com>
2021-05-26 09:02:35 +0000

    Apply fixes from StyleCI

--- tests/BuildCommandTest.php
+++ tests/BuildCommandTest.php
@@ -18,10 +18,12 @@ it('can build the application', function () {
     $composerMock = $this->createMock(ComposerContract::class);
     $this->app->instance(ComposerContract::class, $composerMock);
 
-    $output = new class() extends NullOutput {
+    $output = new class() extends NullOutput
+    {
         public function section()
         {
-            return new class() extends NullOutput {
+            return new class() extends NullOutput
+            {
                 public function clear()
                 {
                 }
@@ -48,10 +50,12 @@ it('reverts the config state after a build', function () {
 
     $contents = File::get(config_path('app.php'));
 
-    $output = new class() extends NullOutput {
+    $output = new class() extends NullOutput
+    {
         public function section()
         {
-            return new class() extends NullOutput {
+            return new class() extends NullOutput
+            {
                 public function clear()
                 {
                     throw new RuntimeException('Foo bar');
