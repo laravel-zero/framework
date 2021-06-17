9657b3001dcfa63ae1b8ad48e13ded798f924247
Dan <dpock32509@gmail.com>
2021-06-16 20:37:43 -0400

    Update StrategyInterface.php

--- src/Components/Updater/Strategy/StrategyInterface.php
+++ src/Components/Updater/Strategy/StrategyInterface.php
@@ -5,4 +5,6 @@ namespace LaravelZero\Framework\Components\Updater\Strategy;
 interface StrategyInterface extends \Humbug\SelfUpdate\Strategy\StrategyInterface
 {
     public function setPackageName($name);
+    
+    public function setCurrentLocalVersion($version);
 }
