3a34cf6f7ce76b1ffbe01f984f600994c4e51848
Nuno Maduro <enunomaduro@gmail.com>
2022-01-13 22:19:31 +0000

    Updates logging stub

--- src/Components/Log/stubs/logging.php
+++ src/Components/Log/stubs/logging.php
@@ -50,7 +50,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['stderr'],
             'ignore_exceptions' => false,
         ],
 
