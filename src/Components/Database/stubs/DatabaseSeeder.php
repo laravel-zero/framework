1558286f1eb08e080f789a8aee0442388cd3b43f
Nuno Maduro <enunomaduro@gmail.com>
2022-01-13 21:48:38 +0000

    Updates database seeder

--- src/Components/Database/stubs/DatabaseSeeder.php
+++ src/Components/Database/stubs/DatabaseSeeder.php
@@ -14,6 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // \YourModel::factory(10)->create();
     }
 }
