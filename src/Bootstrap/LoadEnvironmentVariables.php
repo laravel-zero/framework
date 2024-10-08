f9f6ae1c94bccd6fe8806bcb4a11164d6258427c
Caen De Silva <caen@desilva.se>
2024-10-08 16:10:54 +0200

    style: remove extra newline from source code (#500)

--- src/Bootstrap/LoadEnvironmentVariables.php
+++ src/Bootstrap/LoadEnvironmentVariables.php
@@ -31,7 +31,6 @@ final class LoadEnvironmentVariables implements BootstrapperContract
     public function bootstrap(Application $app): void
     {
         if (class_exists(Dotenv::class)) {
-
             if (file_exists($app->environmentFilePath())) {
                 $app->make(BaseLoadEnvironmentVariables::class)->bootstrap($app);
             }
